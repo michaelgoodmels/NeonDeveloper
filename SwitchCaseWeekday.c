e4de5e8757ce70f0fd317b0f32eeca3ad83758ae
michaelgoodmels <git@good.li>
2024-10-11 11:50:42 +0200

    Day of the week

--- SwitchCaseWeekday.c
+++ SwitchCaseWeekday.c
@@ -1,6 +1,6 @@
 /*
- Project: SwitchCase
- autor: Michael Good
+Project: SwitchCase
+ author: Michael Good
  date: 10.10.2024
  version: 0.0.1
  contact: michael.good@bzbs.ch
@@ -8,43 +8,65 @@
 
 //includes
 #include <stdio.h>
+#include <string.h>
 
 //global Variables
-    int tag = 0;
+int tag = 0;
 
 //Function Prototyping
 void SwitchCase();
 
 //Main Program
- int main(void){
-   SwitchCase();
-   return 0;
- }
+int main(void){
+    SwitchCase();
+    return 0;
+}
 
- //Functions
+//Functions
 void SwitchCase(){
 
-//local Variables in Function
-  int day;
+    //local Variables in Function
+    char day[20];  // To store user input
 
-  printf("\npleace give the day you want see:");
-  scanf("%d", &day);
+    printf("Please enter the day you want to see: ");
+    scanf("%s", day);
 
-    switch (day){
-        case 01: printf("It's a Monday\n: ");
+    // Convert the input string to a corresponding integer
+    if (strcmp(day, "Monday") == 0 || strcmp(day, "monday") == 0) {
+        tag = 1;
+    } else if (strcmp(day, "Tuesday") == 0 || strcmp(day, "tuesday") == 0) {
+        tag = 2;
+    } else if (strcmp(day, "Wednesday") == 0 || strcmp(day, "wednesday") == 0) {
+        tag = 3;
+    } else if (strcmp(day, "Thursday") == 0 || strcmp(day, "thursday") == 0) {
+        tag = 4;
+    } else if (strcmp(day, "Friday") == 0 || strcmp(day, "friday") == 0) {
+        tag = 5;
+    } else if (strcmp(day, "Saturday") == 0 || strcmp(day, "saturday") == 0) {
+        tag = 6;
+    } else if (strcmp(day, "Sunday") == 0 || strcmp(day, "sunday") == 0) {
+        tag = 7;
+    } else {
+        tag = 0;  // Invalid day
+    }
+
+    // Use switch-case with the integer 'tag'
+    switch (tag) {
+        case 1: printf("It's the first day of the week\n");
         break;
-        case 02: printf("It's a Tuesday\n: ");
+        case 2: printf("It's the second day of the week\n");
         break;
-        case 03: printf("It's a Wednesday\n: ");
+        case 3: printf("It's the third day of the week\n");
         break;
-        case 04: printf("It's a Thursday\n: ");
+        case 4: printf("It's the fourth day of the week\n");
         break;
-        case 05: printf("It's a Friday\n: ");
+        case 5: printf("It's the fifth day of the week\n");
         break;
-        case 06: printf("It's a Saturday\n: ");
+        case 6: printf("It's the sixth day of the week\n");
         break;
-        case 07: printf("It's a Sunday\n: ");
+        case 7: printf("It's the seventh day of the week\n");
         break;
-        default: printf("You entered an invalid day");
+        default: printf("You entered an invalid day\n");
+        break;
     }
 }
