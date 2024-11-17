10e169861ab2908c5e6c7816a4879cc86b3aa60b
michaelgoodmels <git@good.li>
2024-11-17 17:16:55 +0100

    first commit

--- SwitchCaseWeekday.c
+++ SwitchCaseWeekday.c
@@ -17,9 +17,11 @@ int tag = 0;
 void SwitchCase();
 
 //Main Program
-int main(void){
-    SwitchCase();
-    return 0;
+int main(void) {
+    while(1) {
+        SwitchCase();
+        //return 0;
+    }
 }
 
 //Functions
@@ -28,45 +30,45 @@ void SwitchCase(){
     //local Variables in Function
     char day[20];  // To store user input
 
-    printf("Please enter the day you want to see: ");
+    printf("\n gib den Tag ein, den du sehen moechtest: ");
     scanf("%s", day);
 
     // Convert the input string to a corresponding integer
-    if (strcmp(day, "Monday") == 0 || strcmp(day, "monday") == 0) {
+    if (strcmp(day, "Montag") == 0 || strcmp(day, "montag") == 0) {
         tag = 1;
-    } else if (strcmp(day, "Tuesday") == 0 || strcmp(day, "tuesday") == 0) {
+    } else if (strcmp(day, "Dienstag") == 0 || strcmp(day, "dienstag") == 0) {
         tag = 2;
-    } else if (strcmp(day, "Wednesday") == 0 || strcmp(day, "wednesday") == 0) {
+    } else if (strcmp(day, "Mittwoch") == 0 || strcmp(day, "mittwoch") == 0) {
         tag = 3;
-    } else if (strcmp(day, "Thursday") == 0 || strcmp(day, "thursday") == 0) {
+    } else if (strcmp(day, "Donnerstag") == 0 || strcmp(day, "donnerstag") == 0) {
         tag = 4;
-    } else if (strcmp(day, "Friday") == 0 || strcmp(day, "friday") == 0) {
+    } else if (strcmp(day, "Freitag") == 0 || strcmp(day, "freitag") == 0) {
         tag = 5;
-    } else if (strcmp(day, "Saturday") == 0 || strcmp(day, "saturday") == 0) {
+    } else if (strcmp(day, "Samstag") == 0 || strcmp(day, "samstag") == 0) {
         tag = 6;
-    } else if (strcmp(day, "Sunday") == 0 || strcmp(day, "sunday") == 0) {
+    } else if (strcmp(day, "Sonntag") == 0 || strcmp(day, "sonntag") == 0) {
         tag = 7;
     } else {
-        tag = 0;  // Invalid day
+        tag = 0;  // falscher Tag
     }
 
     // Use switch-case with the integer 'tag'
     switch (tag) {
-        case 1: printf("It's the first day of the week\n");
+        case 1: printf("Dies ist der erste Tag der Woche\n");
         break;
-        case 2: printf("It's the second day of the week\n");
+        case 2: printf("Das ist der zweite Tag der Woche\n");
         break;
-        case 3: printf("It's the third day of the week\n");
+        case 3: printf("Das ist der dritte Tag der Woche\n");
         break;
-        case 4: printf("It's the fourth day of the week\n");
+        case 4: printf("Das ist der vierte Tag der Woche\n");
         break;
-        case 5: printf("It's the fifth day of the week\n");
+        case 5: printf("Das ist der fuenfte Tag der Woche\n");
         break;
-        case 6: printf("It's the sixth day of the week\n");
+        case 6: printf("Das ist der sechte Tag der Woche\n");
         break;
-        case 7: printf("It's the seventh day of the week\n");
+        case 7: printf("Das ist der siebte Tag der Woche\n");
         break;
-        default: printf("You entered an invalid day\n");
+        default: printf("Kennst du die Tage der Woche?\n");
         break;
     }
 }
